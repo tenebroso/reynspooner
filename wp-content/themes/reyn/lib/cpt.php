1fcb00e1525ddfed5233a56bcecb45ef9c3869ea
Jon Bukiewicz <jonathan@tenebroso.net>
2015-08-04 21:26:40 -0500

    Fix issue with CPT

--- wp-content/themes/reyn/lib/cpt.php
+++ wp-content/themes/reyn/lib/cpt.php
@@ -3,27 +3,27 @@
 function lookbook_cpt() {
 
 	$labels = array(
-		'name'                => _x( 'Lookbooks', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'Lookbook', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'Lookbooks', 'text_domain' ),
-		'name_admin_bar'      => __( 'Lookbook', 'text_domain' ),
-		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-		'all_items'           => __( 'All Items', 'text_domain' ),
-		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
-		'add_new'             => __( 'Add New', 'text_domain' ),
-		'new_item'            => __( 'New Item', 'text_domain' ),
-		'edit_item'           => __( 'Edit Item', 'text_domain' ),
-		'update_item'         => __( 'Update Item', 'text_domain' ),
-		'view_item'           => __( 'View Item', 'text_domain' ),
-		'search_items'        => __( 'Search Item', 'text_domain' ),
-		'not_found'           => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+		'name'                => _x( 'Lookbooks', 'Post Type General Name', 'reynspooner' ),
+		'singular_name'       => _x( 'Lookbook', 'Post Type Singular Name', 'reynspooner' ),
+		'menu_name'           => __( 'Lookbooks', 'reynspooner' ),
+		'name_admin_bar'      => __( 'Lookbook', 'reynspooner' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'reynspooner' ),
+		'all_items'           => __( 'All Items', 'reynspooner' ),
+		'add_new_item'        => __( 'Add New Item', 'reynspooner' ),
+		'add_new'             => __( 'Add New', 'reynspooner' ),
+		'new_item'            => __( 'New Item', 'reynspooner' ),
+		'edit_item'           => __( 'Edit Item', 'reynspooner' ),
+		'update_item'         => __( 'Update Item', 'reynspooner' ),
+		'view_item'           => __( 'View Item', 'reynspooner' ),
+		'search_items'        => __( 'Search Item', 'reynspooner' ),
+		'not_found'           => __( 'Not found', 'reynspooner' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'reynspooner' ),
 	);
 	$args = array(
-		'label'               => __( 'post_type', 'text_domain' ),
-		'description'         => __( 'Lookbooks', 'text_domain' ),
+		'label'               => __( 'lookbook', 'reynspooner' ),
+		'description'         => __( 'Lookbooks', 'reynspooner' ),
 		'labels'              => $labels,
-		'supports'            => array( 'title', ),
+		'supports'            => array( 'title' ),
 		'hierarchical'        => false,
 		'public'              => true,
 		'show_ui'             => true,
@@ -37,9 +37,9 @@ function lookbook_cpt() {
 		'publicly_queryable'  => true,
 		'capability_type'     => 'page',
 	);
-	register_post_type( 'post_type', $args );
+	register_post_type( 'lookbook', $args );
 
 }
 
 // Hook into the 'init' action
-add_action( 'init', 'lookbook_cpt', 0 );
+add_action( 'init', 'lookbook_cpt', 10 );
