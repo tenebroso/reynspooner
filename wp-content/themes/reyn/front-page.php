77e3a40b8f55b64b0429240ba6d0e34eb7c7a83d
Jon Bukiewicz <jonathan@tenebroso.net>
2015-09-16 19:36:02 -0500

    Lots of homepage changes

--- wp-content/themes/reyn/front-page.php
+++ wp-content/themes/reyn/front-page.php
@@ -12,40 +12,25 @@
     </a>
   <?php endwhile; endif; ?>
 
-    <!-- <a href="#" class="home--slider-panel" style="background-image:url(/wp-content/themes/reyn/assets/images/slider-image-1-1600.jpg)">
-      <div class="home--slider-caption">
-        <h1>Aloha for your everyday</h1>
-        <p class="home--slider-cta inline-block">
-          Shop our modern collection <i class="fa fa-angle-right"></i>
-        </p>
-      </div>
-    </a>
-    <a href="#" class="home--slider-panel" style="background-image:url(/wp-content/themes/reyn/assets/images/slider-image-1-1600.jpg)">
-      <div class="home--slider-caption">
-        <h1>Aloha for your everyday</h1>
-        <p class="home--slider-cta inline-block">
-          Shop our modern collection <i class="fa fa-angle-right"></i>
-        </p>
-      </div>
-    </a> -->
   </div>
-  <a class="cta-box--promo zoom-on-hover background-cover">
+  
+  <a class="cta-box--promo zoom-on-hover background-cover" href="<?php the_field('promo_stripe_link'); ?>">
     <div class="cta-box--content">
-      <span class="btn btn-block btn-large">Free Shipping on all orders over $50 <i class="fa fa-angle-right"></i></span>
+      <?php the_field('promo_stripe'); ?>
     </div>
   </a>
+
   <div class="container">
 
     <div class="row">
       <div class="col-sm-12 text-center">
-        <h2 class="brand-text">New Arrivals</h2>
-        <a class="btn btn-default line-through inline-block" href="#">Shop All Kids <i class="fa fa-angle-right"></i></a>
+        <?php the_field('featured_product_promo_heading'); ?>
       </div>
     </div>
      <div class="row">
       <div class="col-xs-12">
-        <a style="background-image:url(/wp-content/themes/reyn/assets/images/product-wide.jpg" class="cta-box--product zoom-on-hover background-cover" href="#">
-          <span class="cta-box--content cta-box--product-details">Caribbean Reef tee<hr /><em>$98</em></span>
+        <a style="background-image:url(<?php the_field('featured_product_center_image'); ?>);" class="cta-box--product zoom-on-hover background-cover" href="<?php the_field('featured_product_center_link'); ?>">
+          <?php the_field('featured_product_center_text'); ?>
         </a>
       </div>
     </div>
@@ -54,56 +39,40 @@
       <div class="col-sm-6">
         <div class="row">
           <div class="col-sm-12 text-center">
-            <h2 class="brand-text">Island Life</h2>
-            <a class="btn btn-default line-through inline-block" href="#">Shop Women's &amp; Accessories <i class="fa fa-angle-right"></i></a>
+            <?php the_field('left_column_product_heading'); ?>
           </div>
         </div>
-        <a style="background-image:url(/wp-content/themes/reyn/assets/images/product-home-womens.jpg" class="cta-box--product zoom-on-hover background-cover" href="#">
-          <span class="cta-box--content cta-box--product-details">Caribbean Reef tee<hr /><em>$98</em></span>
+        <a style="background-image:url(<?php the_field('left_column_product_image'); ?>);" class="cta-box--product zoom-on-hover background-cover" href="<?php the_field('left_column_product_link'); ?>">
+          <?php the_field('left_column_product_text'); ?>
         </a>
       </div>
       <div class="col-sm-6">
         <div class="row">
           <div class="col-sm-12 text-center">
-            <h2 class="brand-text">Surf's Up</h2>
-            <a class="btn btn-default line-through inline-block" href="#">Shop Swim 2015 <span>&gt;</span></a>
+            <?php the_field('right_column_product_heading'); ?>
           </div>
         </div>
-        <a style="background-image:url(/wp-content/themes/reyn/assets/images/product-home-swim.jpg" class="cta-box--product zoom-on-hover background-cover" href="#">
-          <span class="cta-box--content cta-box--product-details">Caribbean Reef tee<hr /><em>$98</em></span>
+        <a style="background-image:url(<?php the_field('right_column_product_image'); ?>);" class="cta-box--product zoom-on-hover background-cover" href="<?php the_field('right_column_product_link'); ?>">
+          <?php the_field('right_column_product_text'); ?>
         </a>
       </div>
     </div>
 
 
   	<div class="row cta-box--three-up">
-  		<div class="col-xs-4">
-        <a href="#" class="cta-box--floral zoom-on-hover text-center background-cover">
-          <div class="cta-box--content v-centered">
-            <h4>Spread Aloha</h4>
-            <hr />
-            <span class="btn btn-block btn-default" href="#">Shop Gift Cards</span>
-          </div>
-        </a>
-      </div>
-      <div class="col-xs-4">
-        <a href="#" class="cta-box--floral bg-alt zoom-on-hover text-center background-cover">
-          <div class="cta-box--content v-centered">
-            <h4>Spread Aloha</h4>
-            <hr />
-            <span class="btn btn-block btn-default" href="#">Shop Gift Cards</span>
-          </div>
-        </a>
-      </div>
-      <div class="col-xs-4">
-        <a href="#" class="cta-box--floral bg-reversed zoom-on-hover text-center background-cover">
-          <div class="cta-box--content v-centered">
-            <h4>Spread Aloha</h4>
-            <hr />
-            <span class="btn btn-block btn-default" href="#">Shop Gift Cards</span>
-          </div>
-        </a>
-      </div>
+
+      <?php if( have_rows('call_to_action_buttons') ): while( have_rows('call_to_action_buttons') ): the_row(); ?>
+
+        		<div class="col-xs-4">
+              <a href="<?php the_sub_field('button_link'); ?>" class="cta-box--floral zoom-on-hover text-center background-cover">
+                <div class="cta-box--content v-centered">
+                  <?php the_sub_field('button_content'); ?>
+                </div>
+              </a>
+            </div>
+
+      <?php endwhile; endif; ?>
+
     </div>
 
 
@@ -127,7 +96,7 @@
     <div class="row">
       <div class="col-sm-12 text-center">
         <h2>From Our Blog</h2>
-        <a class="btn btn-default line-through inline-block" href="#">See All Posts <i class="fa fa-angle-right"></i></a>
+        <a class="btn btn-default line-through inline-block" href="/blog/">See All Posts <i class="fa fa-angle-right"></i></a>
       </div>
 
 <?php
