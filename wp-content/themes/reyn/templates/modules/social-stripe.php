1177d253bcc4b53c7ae38e1c242880872fc96890
Jon Bukiewicz <jonathan@tenebroso.net>
2015-09-24 13:05:02 -0500

    Fix to general content view

--- wp-content/themes/reyn/templates/modules/social-stripe.php
+++ wp-content/themes/reyn/templates/modules/social-stripe.php
@@ -1,8 +1,8 @@
 <div class="social-stripe">
   <div class="v-centered">
-    <a href="#"><i class="fa fa-facebook-square"></i> Facebook</a>
-    <a href="#"><i class="fa fa-pinterest-square"></i> Pinterest</a>
-    <a href="#"><i class="fa fa-twitter-square"></i> Twitter</a>
-    <a href="#"><i class="fa fa-instagram"></i> Instagram</a>
+    <a href="https://www.facebook.com/reynspooner" target="_blank"><i class="fa fa-facebook-square"></i> Facebook</a>
+    <a href="https://www.pinterest.com/reynspooner/" target="_blank"><i class="fa fa-pinterest-square"></i> Pinterest</a>
+    <a href="https://twitter.com/reynspooner" target="_blank"><i class="fa fa-twitter-square"></i> Twitter</a>
+    <a href="https://instagram.com/reynspooner/" target="_blank"><i class="fa fa-instagram"></i> Instagram</a>
   </div>
 </div>
