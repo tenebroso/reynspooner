0f19b113422a31701356d92a1a69938f270a5793
Jon Bukiewicz <jbukiewicz@devnext.net>
2015-12-17 10:28:26 -0600

    Fixes

--- wp-content/themes/reyn/templates/searchform.php
+++ wp-content/themes/reyn/templates/searchform.php
@@ -1,4 +1,4 @@
 <form class="search-form form-inline" action="http://reyn-spooner-com.myshopify.com/search">
   <button type="submit" class="search-submit btn btn-default"><i class="fa fa-search"></i></button>
-  <input type="search" name="q" class="search-field form-control" placeholder="Search"  />
+  <input type="search" name="q" class="search-field form-control" placeholder="Search" maxlength="30"  />
 </form>
