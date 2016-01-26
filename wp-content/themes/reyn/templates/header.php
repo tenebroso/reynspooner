46930e0153bcc1b180719218a2e307f4a2dcd9c9
Jon Bukiewicz <jbukiewicz@devnext.net>
2016-01-25 22:48:16 -0500

    Update mobile nav search form

--- wp-content/themes/reyn/templates/header.php
+++ wp-content/themes/reyn/templates/header.php
@@ -310,7 +310,7 @@
     <div class="mobile-search">
       <form class="search-form form-inline" action="http://reyn-spooner-com.myshopify.com/search">
           <button type="submit" class="search-submit btn btn-default"><i class="fa fa-search"></i></button>
-          <input type="search" name="q" class="search-field form-control" placeholder="Search" maxlength="30" />
+          <input type="search" name="q" class="search-field form-control" placeholder="Search print, product, color, etc ..." maxlength="30" />
       </form>
     </div>
     <ul id="menu-mobile-navigation" class="nav">
