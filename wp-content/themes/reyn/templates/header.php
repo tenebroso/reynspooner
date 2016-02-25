f79b03705087ec02a5cd1277c2926ea69c540791
Abel Quitoriano <abel@loftynewmedia.com>
2016-02-26 06:40:19 +0800

    remove devclass-test

--- wp-content/themes/reyn/templates/header.php
+++ wp-content/themes/reyn/templates/header.php
@@ -378,7 +378,7 @@
   </div>
   <div class="mobile-navigation">
     <div class="mobile-search">
-      <form class="search-form form-inline devclass-test" action="https://shop.reynspooner.com/search">
+      <form class="search-form form-inline" action="https://shop.reynspooner.com/search">
           <button type="submit" class="search-submit btn btn-default"><i class="fa fa-search"></i></button>
           <input type="search" name="q" class="search-field form-control" placeholder="Search print, product, color, etc ..." maxlength="30" />
       </form>
